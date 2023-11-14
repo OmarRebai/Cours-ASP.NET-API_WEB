fd054e6355ac947b14262efd84de86ebeed0b8be
omar <omar.rebai@enis.tn>
2023-11-14 10:59:19 +0100

    Ajoutez des fichiers projet.

---
+++ Models/Department.cs
@@ -0,0 +1,8 @@
+﻿namespace TP5_ASP.NET_API_WEB.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+    }
+}
---
+++ Models/Gender.cs
@@ -0,0 +1,7 @@
+﻿namespace TP5_ASP.NET_API_WEB.Models
+{
+    public enum Gender
+    {
+        Male,Female
+    }
+}
